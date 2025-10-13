6c4ce73d049b595e355e4daa9bfdcca5f0a8be7f
dsaini64 <dsaini64@gmail.com>
2025-10-13 14:09:39 -0700

    Initial Commit

---
+++ foodtrackerv2/foodtrackerv2App.swift
@@ -0,0 +1,17 @@
+//
+//  foodtrackerv2App.swift
+//  foodtrackerv2
+//
+//  Created by Divakar Saini on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct foodtrackerv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
